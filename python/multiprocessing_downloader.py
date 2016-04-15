c5ce2f32ad30b9c5d29f7c9f43cf5286abecbe45
MrLokans <trikster1911@gmail.com>
2016-04-15 20:50:11 +0300

    Process pool added. Parallel download implemented.

--- python/multiprocessing_downloader.py
+++ python/multiprocessing_downloader.py
@@ -3,6 +3,7 @@ import os
 import logging
 
 from collections import namedtuple
+from multiprocessing import Pool
 
 import lxml.html
 import requests
@@ -11,12 +12,13 @@ import requests
 import settings as pref
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 Book = namedtuple('Book', ('title', 'description',
                            'authors', 'url',
                            'year', 'publisher',
-                           'pages', 'ISBN'))
+                           'pages', 'ISBN',
+                           'format', 'origin_url'))
 
 
 def extract_id_from_text(text):
@@ -54,7 +56,10 @@ def prepare_dir(download_dir=pref.DOWNLOAD_DIR):
 
 def parse_book_from_url(url):
     r = requests.get(url, headers={'User-Agent': pref.CUSTOM_USERAGENT})
-    return _parse_book_from_text(r.text)
+    if not r.status_code == 200:
+        logging.info("Incorrect status code: {}".format(r.status_code))
+        return None
+    return _parse_book_from_text(r.text, url)
 
 
 def get_first_selector_value(tree, selector, sel_type='css'):
@@ -71,7 +76,7 @@ def get_first_selector_value(tree, selector, sel_type='css'):
         return elements[0]
 
 
-def _parse_book_from_text(text):
+def _parse_book_from_text(text, origin_url):
     tree = lxml.html.fromstring(text)
 
     book_title = get_first_selector_value(tree, pref.TITLE_SELECTOR).text
@@ -80,6 +85,7 @@ def _parse_book_from_text(text):
     book_year = get_first_selector_value(tree, pref.YEAR_SELECTOR).text
     book_pages = get_first_selector_value(tree, pref.PAGES_SELECTOR).text
     book_authors = get_first_selector_value(tree, pref.AUTHORS_SELECTOR).text
+    book_format = get_first_selector_value(tree, pref.FORMAT_SELECTOR).text
 
     return Book(title=book_title,
                 description="",
@@ -88,13 +94,48 @@ def _parse_book_from_text(text):
                 year=book_year,
                 publisher="",
                 pages=book_pages,
-                ISBN="")
+                ISBN="",
+                format=book_format,
+                origin_url=origin_url)
+
+
+def download_book(book_obj):
+    if book_obj is None:
+        pass
+    logging.info("Downloading {}".format(book_obj.title))
+    print(book_obj)
+
+
+def download_book_by_url(book_url):
+    book = parse_book_from_url(book_url)
+    if book is None:
+        logging.info("Url {} contains no book data!".format(book_url))
+    logging.info("Book obtained {}".format(book))
+    output_filename = os.path.join(pref.DOWNLOAD_DIR,
+                                   ".".join([book.title, book.format]))
+    download_file(book.url, output_filename, book_url)
+
+
+def download_file(url, output_file, original_url):
+    headers = {
+        "Referer": original_url
+    }
+    r = requests.get(url, headers=headers, stream=True)
+    with open(output_file, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+    return output_file
 
 
 def main():
     print(get_latest_book_id())
-    parse_book_from_url(get_book_id_url(20))
-    prepare_dir()
+    # books = (parse_book_from_url(u) for u in get_book_url_generator(1, 10))
+    # books = list(books)
+    book_urls = list(get_book_url_generator(1, 50))
+    pool = Pool(6)
+    pool.map(download_book_by_url, book_urls)
+    # prepare_dir()
 
 
 if __name__ == '__main__':
